e76aa40fbf3652eac2cce479fb517dfc180b3d02
ayan4m1 <andrew@bulletlogic.com>
2020-08-26 12:12:01 -0400

    show recently added lists on index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,77 @@
+import { formatDistanceToNow, parseISO, compareDesc } from 'date-fns';
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
 import React from 'react';
-import { Container, Jumbotron } from 'react-bootstrap';
+import { Container, Jumbotron, Row, Card, Col, Button } from 'react-bootstrap';
 
 import Layout from '~components/Layout';
 import SEO from '~components/SEO';
+import { graphql } from 'gatsby';
 
-export default function IndexPage() {
+export default function IndexPage({ data }) {
+  const lists = data.allListsJson.nodes;
+
+  lists.sort((a, b) =>
+    compareDesc(parseISO(a.dateAdded), parseISO(b.dateAdded))
+  );
+
   return (
     <Layout>
       <SEO title="Home" />
       <Jumbotron variant="info">
         <Container>
           <h1>The Flavor Page</h1>
+          <h3>Because everyone loves a good list.</h3>
         </Container>
       </Jumbotron>
+      <h4>New Lists</h4>
+      {lists.map((list) => (
+        <Row key={list.code} className="mb-2">
+          <Col>
+            <Card>
+              <Card.Body>
+                <Row>
+                  <Col md={8}>
+                    <h5>
+                      {list.name} - {list.flavors.length} flavors
+                    </h5>
+                    <p>
+                      Added{' '}
+                      {formatDistanceToNow(parseISO(list.dateAdded), {
+                        addSuffix: true
+                      })}
+                    </p>
+                  </Col>
+                  <Col md={4}>
+                    <Link to={`/${list.code}`}>
+                      <Button variant="info">View &raquo;</Button>
+                    </Link>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      ))}
     </Layout>
   );
 }
+
+IndexPage.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+export const pageQuery = graphql`
+  query {
+    allListsJson {
+      nodes {
+        name
+        code
+        dateAdded
+        flavors {
+          flavorName
+        }
+      }
+    }
+  }
+`;
