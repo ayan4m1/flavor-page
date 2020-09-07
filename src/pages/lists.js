94ec525f3e87a2f6a25e35bb34cdca612c32e1ca
ayan4m1 <andrew@bulletlogic.com>
2020-09-06 21:03:49 -0400

    add list of lists

---
+++ src/pages/lists.js
@@ -0,0 +1,63 @@
+import { graphql, Link } from 'gatsby';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Table } from 'react-bootstrap';
+
+import Layout from '~components/Layout';
+
+export default function ListsPage({ data }) {
+  const lists = data.allListsJson.edges.map(({ node }) => node);
+
+  return (
+    <Layout>
+      <h1>Lists</h1>
+      <Table striped hover>
+        <thead>
+          <tr>
+            <th>List Name</th>
+            <th>Author</th>
+            <th>Date Added</th>
+          </tr>
+        </thead>
+        <tbody>
+          {lists.map((list) => (
+            <tr key={list.code}>
+              <td>
+                <Link to={`/${list.code}`}>{list.name}</Link>
+              </td>
+              <td>
+                <a
+                  href={`https://reddit.com/u/${list.author}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {list.author}
+                </a>
+              </td>
+              <td>{list.dateAdded}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </Layout>
+  );
+}
+
+ListsPage.propTypes = {
+  data: PropTypes.object.isRequired
+};
+
+export const pageQuery = graphql`
+  query {
+    allListsJson {
+      edges {
+        node {
+          name
+          code
+          author
+          dateAdded
+        }
+      }
+    }
+  }
+`;
